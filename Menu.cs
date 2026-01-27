bec06a5fb15c7d5d3c4c6e71eefcfccd85e2114c
Paola Mora <paola.moram2005@hotmail.com>
2026-01-26 19:17:58 -0700

    Menu added

---
+++ Menu.cs
@@ -0,0 +1,19 @@
+public class Menu
+{
+    public void ShowWelcomeMessage()
+    {
+        System.Console.WriteLine(@"
+───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█");
+        System.Console.WriteLine("to To-Do List!");
+        System.Console.WriteLine("Please press any key to continue...");
+        Console.ReadKey(true);
+    }
+}
