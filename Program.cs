b244aea8dfca89750402bfab420e699295ab983d
Christopher Hercules <chriherc215@gmail.com>
2026-01-28 11:02:44 -0700

    added main loop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿Menu menu = new Menu();
+﻿using System.Data;
+
+class program {
+    static List<TodoTask> tasks = new List<ThreadPool>();
+static void Main(){
+
+Menu menu = new Menu();
 menu.ShowWelcomeMessage();
+
+    while ( true )
+        {
+            
+            Console.Clear();
+            Console.WriteLine("Todo list");
+            Console.WriteLine("1.add task");
+            Console.WriteLine("2.remove task");
+            Console.WriteLine("3.mark task as complete ");
+            Console.WriteLine("4. veiw tasks ");
+            Console.WriteLine("5. Exit ");
+            Console.WriteLine("5.choose an exit option  ");
+
+
+
+
+        }
+
+
+
+    }
+}
+
